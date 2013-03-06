6137b28fbf9315eaae754b8979dc83c937617eb3
meloniq <meloniq@gmail.com>
2013-03-06 09:51:22 +0100

    hook into packages, localize tooltip

--- wpml-actions.php
+++ wpml-actions.php
@@ -14,7 +14,7 @@ add_filter( 'appthemes_page_id_for_template', 'app_wpml_appthemes_page_id_for_te
  * ClassiPress: hook into cp_get_ad_details()
  */
 function app_wpml_cp_ad_details_field( $result, $post, $location ) {
-	$result->field_label = icl_translate( APP_TD, 'label_' . $result->field_label, $result->field_label );
+	$result->field_label = icl_translate( APP_TD, 'label_' . $result->field_name, $result->field_label );
 	return $result;
 }
 add_filter( 'cp_ad_details_field', 'app_wpml_cp_ad_details_field', 10, 3 );
@@ -24,11 +24,37 @@ add_filter( 'cp_ad_details_field', 'app_wpml_cp_ad_details_field', 10, 3 );
  * ClassiPress: hook into cp_formbuilder(), cp_formbuilder_review()
  */
 function app_wpml_cp_formbuilder_field( $result ) {
-	$result->field_label = icl_translate( APP_TD, 'label_' . $result->field_label, $result->field_label );
+	$result->field_label = icl_translate( APP_TD, 'label_' . $result->field_name, $result->field_label );
+	if ( ! empty( $result->field_tooltip ) )
+		$result->field_tooltip = icl_translate( APP_TD, 'tooltip_' . $result->field_name, $result->field_tooltip );
 	return $result;
 }
 add_filter( 'cp_formbuilder_field', 'app_wpml_cp_formbuilder_field' );
 add_filter( 'cp_formbuilder_review_field', 'app_wpml_cp_formbuilder_field' );
+
+
+/**
+ * ClassiPress: hook into cp_package_field filter
+ */
+function app_wpml_cp_package_field( $result, $type ) {
+	$result->pack_name = icl_translate( APP_TD, 'pack_name_' . $result->pack_id, $result->pack_name );
+	$result->pack_desc = icl_translate( APP_TD, 'pack_desc_' . $result->pack_id, $result->pack_desc );
+	return $result;
+}
+add_filter( 'cp_package_field', 'app_wpml_cp_package_field', 10, 2 );
+
+
+/**
+ * ClassiPress: hook into get_pack()
+ */
+function app_wpml_cp_get_package( $package, $pack_id ) {
+	if ( ! empty( $package ) ) {
+		$package->pack_name = icl_translate( APP_TD, 'pack_name_' . $package->pack_id, $package->pack_name );
+		$package->pack_desc = icl_translate( APP_TD, 'pack_desc_' . $package->pack_id, $package->pack_desc );
+	}
+	return $package;
+}
+add_filter( 'cp_get_package', 'app_wpml_cp_get_package', 10, 2 );
 
 
 /**
